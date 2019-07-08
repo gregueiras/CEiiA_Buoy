5ca06e9b5d1486e6d973d2c6b7565991da0adbbd
Gonçalo Santos <gregueiras@gmail.com>
2019-07-08 14:21:37 +0100

    removed static_root

--- buoy2/settings.py
+++ buoy2/settings.py
@@ -204,6 +204,5 @@ STATIC_URL = '/static/'
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
 os.makedirs(STATIC_TMP, exist_ok=True)
-os.makedirs(STATIC_ROOT, exist_ok=True)
 
 django_heroku.settings(locals())
