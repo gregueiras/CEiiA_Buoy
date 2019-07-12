cc4b78febed23bd58248a43ec7196a6d1356920e
Gonçalo Santos <gregueiras@gmail.com>
2019-07-12 14:35:53 +0100

    updated media "cache"

--- buoy2/settings.py
+++ buoy2/settings.py
@@ -133,8 +133,8 @@ STATICFILES_DIRS = (
     MACHINA_MAIN_STATIC_DIR,
     'favicon',
     'geojson',
-    'faq/static/faq/css',
-    'contact/static/contact/css',
+    'faq/static',
+    'contact/static',
 )
 
 CACHES = {
